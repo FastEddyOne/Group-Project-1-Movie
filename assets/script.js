e355a666717cab2e12d2b72e4f0653decde420c1
kate patch <kate.epatch@gmail.com>
2022-05-26 22:00:23 -0600

    figuring out the local storage logic

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var searchHistory = [];
 
 if(localStorage["searchHistory"]) {
     searchHistory = JSON.parse(locatStorage['searchHistory']);
+    console.log(searchHistory);
 }
 if(searchHistory.indexOf(search) == -1) {
     searchHistory.unshift(search);
@@ -10,3 +11,9 @@ if(searchHistory.indexOf(search) == -1) {
     }
     localStorage['searchHistory'] = JSON.stringify(searchHistory);
 }
+$(".submit").on("click", function(){
+    
+} )
+
+ $("#search-list").val(localStorage.getItem("search-list"));
+
