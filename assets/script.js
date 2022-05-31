ab7d041cdc8598959730a1e8bb3df935bf877b40
kate patch <kate.epatch@gmail.com>
2022-05-31 13:54:06 -0600

    renamed search movies to search list

--- assets/script.js
+++ assets/script.js
@@ -45,12 +45,13 @@ fetch('https://juanroldan1989-moviequotes-v1.p.rapidapi.com/api/v1/quotes?actor=
 	.catch(err => console.error(err));
 
 
+ var searchList = document.getElementById("search-list")   
  var searchHistory = [];
 
  function getItems() {
-     var storedMovies = JSON.parse(localStorage.getItem("searchHistory"));
-     if (storedMovies !== null) {
-         searchHistory = storedMovies;
+     var searchList = JSON.parse(localStorage.getItem("searchHistory"));
+     if (searchList !== null) {
+         searchHistory = searchList;
      };
      for (i=0; i<searchHistory.length; i++) {
          if (i == 10) {
